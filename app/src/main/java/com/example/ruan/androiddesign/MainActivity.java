8747eb0245e24a2c0de344c523073c14ecbc3185
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-20 15:26:03 -0200

    aula 06

--- app/src/main/java/com/example/ruan/androiddesign/MainActivity.java
+++ app/src/main/java/com/example/ruan/androiddesign/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.ruan.androiddesign;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentoRed.OnFragmentInteractionListener, FragmentoAzul.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
