69db7f8b1e51ddb8a95a7b00946440298fd2da97
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-20 16:19:18 -0200

    aula 12

--- app/src/main/java/com/example/ruan/androiddesign/MainActivity.java
+++ app/src/main/java/com/example/ruan/androiddesign/MainActivity.java
@@ -4,12 +4,23 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements FragmentoRed.OnFragmentInteractionListener, FragmentoAzul.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements
+        FragmentoRed.OnFragmentInteractionListener,
+        FragmentoAzul.OnFragmentInteractionListener,
+        FragmentoVerde.OnFragmentInteractionListener {
+
+    FragmentoAzul fragmentoAzul;
+    FragmentoVerde fragmentoVerde;
+    FragmentoRed fragmentoRed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentoAzul = new FragmentoAzul();
+        fragmentoRed = new FragmentoRed();
+        fragmentoVerde = new FragmentoVerde();
     }
 
     @Override
